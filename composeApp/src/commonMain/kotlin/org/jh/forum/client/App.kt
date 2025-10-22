c585fc74b3d08c8883ba667e3284bcff37f07056
SugarMGP <2350745751@qq.com>
2025-10-22 16:02:59 +0800

    fix: android crash

--- composeApp/src/commonMain/kotlin/org/jh/forum/client/App.kt
+++ composeApp/src/commonMain/kotlin/org/jh/forum/client/App.kt
@@ -1,7 +1,11 @@
 package org.jh.forum.client
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import org.jh.forum.client.ui.navigation.MainNavigation
 import org.jh.forum.client.ui.screen.ThemeMode
 import org.jh.forum.client.ui.theme.ForumTheme
@@ -20,10 +24,15 @@ fun App() {
         darkTheme = darkTheme,
         dynamicColor = true // Enable dynamic color on Android 12+
     ) {
-        MainNavigation(
-            onThemeChanged = { mode ->
-                themeState.setThemeMode(mode)
-            }
-        )
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            MainNavigation(
+                onThemeChanged = { mode ->
+                    themeState.setThemeMode(mode)
+                }
+            )
+        }
     }
 }
